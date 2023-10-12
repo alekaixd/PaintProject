f529e16b369532443b017723e625e16647277a4b
alekaixd <aleksi.pirttinen@gmail.com>
2023-10-12 18:37:28 +0300

    added a bunch

---
+++ PainterProject/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //activate level transition screen
+    }
+}
--- PainterProject/Assets/Scripts/NextLevel.cs
+++ PainterProject/Assets/Scripts/NextLevel.cs
@@ -6,21 +6,33 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-private void OnTriggerEnter2D(Collider2D collision)
+    public GameObject levelClearedScreen;
+    public GameObject player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
 {
-    var currentscene = SceneManager.GetActiveScene();
-    if (currentscene == SceneManager.GetSceneByName("Level 1"))
-    {
-        SceneManager.LoadScene("Level 2");
-    }
-    if (currentscene == SceneManager.GetSceneByName("Level 2"))
-    {
-        SceneManager.LoadScene("Level 3");
-    }
-    if (currentscene == SceneManager.GetSceneByName("Level 3"))
-    {
-        GameObject.Find("MUSIC MAN").GetComponent<MusicClass>().StopMusic();
-        SceneManager.LoadScene("Endscreen");
-    }
+    if (collision.CompareTag("Player"))
+        {
+            levelClearedScreen.SetActive(true);
+        }
+    
 }
+
+    public void LoadNextLevel()
+    {
+        var currentscene = SceneManager.GetActiveScene();
+        if (currentscene == SceneManager.GetSceneByName("Level 1"))
+        {
+            SceneManager.LoadScene("Level 2");
+        }
+        if (currentscene == SceneManager.GetSceneByName("Level 2"))
+        {
+            SceneManager.LoadScene("Level 3");
+        }
+        if (currentscene == SceneManager.GetSceneByName("Level 3"))
+        {
+            GameObject.Find("MUSIC MAN").GetComponent<MusicClass>().StopMusic();
+            SceneManager.LoadScene("Endscreen");
+        }
+    }
 }
