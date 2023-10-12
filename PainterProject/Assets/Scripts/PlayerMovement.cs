5c7ae6fe3272da1988406bbf047a7d6753cad444
Mrpekonipvp <Lukaspietila37@gmail.com>
2023-10-12 19:56:15 +0300

    ANIMATIONS

--- PainterProject/Assets/Scripts/PlayerMovement.cs
+++ PainterProject/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 5f;
     private float jumpingPower = 10f;
     private bool isFacingRight = true;
+    private Animator animator;
     public Rigidbody2D rb2d;
     public Transform groundCheck;
     public LayerMask groundlayer;
@@ -15,12 +16,16 @@ public class PlayerMovement : MonoBehaviour
     public AnimationCurve movementCurve;
     public float time;
 
-    
 
+
+    private void Start()
+    {
+        animator= gameObject.GetComponent<Animator>();
+    }
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        
+        animator.SetFloat("Hor", horizontal);
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpingPower);
@@ -29,8 +34,9 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonUp("Jump") && rb2d.velocity.y > 0f)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
+            
         }
-
+        animator.SetFloat("Ver", rb2d.velocity.normalized.y);
         if (Input.GetButton("Horizontal"))
         {
             speed = movementCurve.Evaluate(time);
